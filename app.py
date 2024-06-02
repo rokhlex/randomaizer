7c01861854ccc86b869cd15cec7ccfdc31d4f1f6
rokhlex <153430110+rokhlex@users.noreply.github.com>
2024-06-02 09:26:33 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+import random
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+choices = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', choices=choices)
+
+@app.route('/random')
+def random_choice():
+    if choices:
+        random_choice = random.choice(choices)
+        return render_template('result.html', choice=random_choice)
+    else:
+        flash('Список вариантов пуст. Пожалуйста, добавьте варианты!', 'warning')
+        return redirect(url_for('index'))
+
+@app.route('/clear', methods=['GET'])
+def clear_choices():
+    global choices
+    choices = []
+    flash('Все варианты удалены!', 'info')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
